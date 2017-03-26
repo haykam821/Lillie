eec9aa7d90a0a3a2305a7de44ff7af41dadd7522
Mega-Mewthree <vietbaotran530@gmail.com>
2017-03-25 18:31:53 -0700

    Update botPrototype.js

--- botPrototype.js
+++ botPrototype.js
@@ -334,11 +334,11 @@ bot.on("message", msg => {
     let target = spy.guilds.get("252525368865456130");
     let numShared = 0;
     origin.members.forEach((m)=>{
-      if (target.member(m)){
+      if (target.member(m).id > 0){
         numShared++;
       }
     });
-    msg.channel.sendMessage(numShared + " members are in SK! (" + numShared / origin.memberCount + "%)");
+    msg.channel.sendMessage(numShared + " members are in SK! (" + (numShared / origin.memberCount) + "%)");
   }
 
   else if (command == "nebulayt"){
