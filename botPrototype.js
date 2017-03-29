7e1dd0c972fe148335e20f0c69222da5c5edbdbf
Mega-Mewthree <vietbaotran530@gmail.com>
2017-03-28 20:49:07 -0700

    Update botPrototype.js

--- botPrototype.js
+++ botPrototype.js
@@ -1,4 +1,4 @@
-var global = {moo: {logChannel: "296430168141201410", name: "Lillie", alliance: "Nebula", acceptAll: false, requestTimeout: 300000}, partyLink: "52.39.54.145"
+var global = {moo: {logChannel: "296430168141201410", name: "Lillie", alliance: "Nebula", acceptAll: false, requestTimeout: 300000, partyLink: "52.39.54.145"}}
 
 var globalChannel = "296429968555114526";
 var globalMsgID = "296430530575204352";
